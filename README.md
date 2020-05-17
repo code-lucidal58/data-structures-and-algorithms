77b2b006084c03a9af40bd9808da9edffce86b0a
code-lucidal58 <aanisha.mishra05@gmail.com>
2020-05-17 20:03:51 +0530

    naive algo added

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 I am learning new algorithms and will be making notes for all of thoses here, along with examples.
 
 * Kabane Alhorithm
+
+String matching algorithms
+* Naive
 * KMP (Knuth Morris Pratt) Pattern Searching
---
+++ naive.py
@@ -0,0 +1,26 @@
+"""
+Naive algorithm is used for finding the indices of the presence of a given substring pattern in a string
+Time Complexity O(m*(n-m+1)) or O(nm) where n and m are the lengths of string snd substrings respectively
+"""
+
+
+def naive(s: str, pat: str):
+    n = len(s)
+    m = len(pat)
+    l = []
+    i = 0
+    while i < (n - m + 1):
+        j = 0
+        while j < m and s[i + j] == pat[j]:
+            j += 1
+        if j == m:
+            l.append(i)
+        i += 1
+    return l
+
+
+if __name__ == '__main__':
+    print(naive("AABAACAADAABAABA", "AABA"))
+    print(naive("AABCCAADDEE", "FAA"))
+    print(naive("AAAAAAAAAAAAAAAAAA", "AAAAA"))  # worst case
+    print(naive("AAAAAAAAAAAAAAAAAB", "AAAAB"))  # worst case
